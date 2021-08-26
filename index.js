9b2c297c600f85d6fe69929c212b58fe05f8d1da
v.rysin <666728666zbk>
2021-08-26 18:46:30 +0400

    workers

--- index.js
+++ index.js
@@ -1,97 +1,14 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const { StringDecoder } = require('string_decoder');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-const config = require('./config');
-const handlers = require('./lib/handlers')
+const app = {};
 
-// Test
-// @TODO remove this
-// _data.create('test', 'newFile.json', { 0: "create test" });
-// _data.read('test', 'newFile.json', (err, data) => { console.log(err, data) });
-// _data.update('test', 'newFile.json', { 1: "update test" });
-// _data.delete('test', 'newFile.json');
-
-const { httpPort, httpsPort } = config;
-
-const server = (req, res) => {
-	// Get the URL and parse it
-	const parsedUrl = url.parse(req.url, true);
-
-	// regular expression to trim slashes
-	const trimRegExp = /^\/+|\/+$/g;
-
-	// Get the path
-	const path = parsedUrl.pathname;
-	const trimmedPath = path.replace(trimRegExp, '');
-
-	//Get query string as an object
-	const { query } = parsedUrl;
-
-	// req destructure
-	const { method, headers } = req;
-
-	// Get the payload, if any
-	const decoder = new StringDecoder('utf-8');
-	let buffer = '';
-	req.on('data', data => {
-		buffer += decoder.write(data)
-	});
-	req.on('end', () => {
-		buffer += decoder.end();
-		const chosenHandler = router[trimmedPath] || router.notFound;
-		const data = {
-			trimmedPath,
-			query,
-			method,
-			headers,
-			payload: buffer && JSON.parse(buffer)
-		};
-		chosenHandler(data, (statusCode = 200, payload = {}) => {
-			const payloadString = JSON.stringify(payload);
-			res.setHeader('Content-Type', 'application/json')
-			res.writeHead(statusCode);
-			res.end(payloadString);
-			console.log(
-				'Returning this response: ',
-				statusCode,
-				payloadString
-			)
-		});
-	});
-}
-const httpServer = http.createServer((req, res) => {
-	server(req, res);
-});
-
-httpServer.listen(httpPort, () => {
-	console.log(
-		`The server is listening on port ${httpPort}`
-	);
-});
-
-const httpsServerOptions = {
-	'key': fs.readFileSync('./https/key.pem'),
-	'cert': fs.readFileSync('./https/cert.pem'),
+app.init = () => {
+	server.init();
+	workers.init();
 };
-const httpsServer = https.createServer(httpsServerOptions,(req, res) => {
-	server(req, res);
-});
 
-httpsServer.listen(httpsPort, () => {
-	console.log(
-		`The server is listening on port ${httpsPort}`
-	);
-});
+app.init();
 
-const router = {
-	'user': handlers.user,
-	'token': handlers.token,
-	'check': handlers.check,
-	// funny services
-	'ping': handlers.ping,
-	'hello': handlers.hello,
-	'notFound': handlers.notFound,
-}
+module.exports = app;
+
