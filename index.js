497f675994a389646a5a605e8f649b230efc4f7f
RysVA <v.rysin@bssys.com>
2022-01-07 12:52:25 +0400

    cli

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 const app = {};
 
 app.init = () => {
 	server.init();
 	workers.init();
+
+	setTimeout(() => {
+    cli.init();
+  }, 50)
 };
 
 app.init();
